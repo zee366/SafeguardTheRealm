bfa67a67978798d048f5b33295a9e0eb90e259d1
Isaac Doré <swessmusic@gmail.com>
2019-10-14 16:50:54 -0400

    Beginning of Spawner

---
+++ Assets/_Scripts/Utils/Spawnable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct Spawnable {
+
+    public GameObject prefab;
+    public float      probability;
+
+}
---
+++ Assets/_Scripts/Utils/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils {
+    public class Spawner : MonoBehaviour {
+
+        [SerializeField] private float           _interval = 1f;
+        public                   List<Spawnable> spawnables;
+
+
+        public void SpawnOne() {
+
+        }
+
+        
+    }
+}
