bab339ec25753042e13c0ddb0bdd0b420415128c
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2019-10-16 16:09:28 -0400

    Progress on towers

---
+++ Assets/_Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+
+    private GameObject enemy;
+     
+
+    public void setEnemy(GameObject go) { enemy = go; }
+
+
+    void Update() {
+        if ( enemy == null ) return;
+        MoveTowardsEnemy();
+    }
+    private void MoveTowardsEnemy() {
+//        float step =  speed * Time.deltaTime; // calculate distance to move
+        float step = 0.1f;
+        transform.position = Vector3.MoveTowards(transform.position, enemy.transform.position, step);
+    }
+
+}
--- Assets/_Scripts/Tower/Tower.cs
+++ Assets/_Scripts/Tower/Tower.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
-using UnityEditor;
+using Events;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
@@ -9,9 +8,18 @@ public class Tower : MonoBehaviour {
 
     [Range(2f, 20f)]
     public float radius;
+    [Range(0.1f, 4f)]
+    public float attackSpeed;
+    public Projectile projectile;
+    public List<Transform> _towerWeaponPosition;
+
+    private GameObject _enemyBeingTargetted;
     private GameObject _radiusObjet;
     private Dictionary< int , GameObject> _enemyInRange;
     private List<Transform> _towerWeaponVisuals;
+    
+    private bool projectileCoroutineStarted = false;
+    private Timer _timer;
 
     void Start() {
         _radiusObjet = transform.Find("Range").gameObject;
@@ -25,14 +33,31 @@ public class Tower : MonoBehaviour {
                 _towerWeaponVisuals.Add(child);
             }
         }
+        
     }
     
 
     // Update is called once per frame
     void Update() {
-        if ( _enemyInRange.Count > 0 ) LookAtEnemy();
+        if ( _enemyInRange.Count > 0 ) {
+            GameObject enemy = findClosestEnemy();
+           _enemyBeingTargetted = enemy;
+           LookAtEnemy();
+           if (!projectileCoroutineStarted) StartCoroutine(ProjectileCoroutine());
+        } else {
+            StopAllCoroutines();
+        }
     }
 
+
+    IEnumerator ProjectileCoroutine() {
+        projectileCoroutineStarted = true;
+        sendProjectile(_enemyBeingTargetted);
+        while ( true ) {
+            yield return new WaitForSeconds(attackSpeed);
+            sendProjectile(_enemyBeingTargetted);
+        }
+    }
     public void AddEnemyToMemory(Transform t) {
         _enemyInRange.Add(t.gameObject.GetInstanceID(), t.gameObject);
     }
@@ -41,22 +66,34 @@ public class Tower : MonoBehaviour {
     public void RemoveEnemyFromMemory(Transform t) {
         _enemyInRange.Remove(t.gameObject.GetInstanceID()); 
     }
-    
 
-    void LookAtEnemy() {
-        Vector3 target = Vector3.positiveInfinity;
+
+    GameObject findClosestEnemy() {
+        GameObject target = null;
         
-        foreach ( KeyValuePair<int, GameObject> IdEnemyPair in _enemyInRange ) {
-            target = IdEnemyPair.Value.transform.position;
+        foreach ( KeyValuePair<int, GameObject> idEnemyPair in _enemyInRange ) {
+            target = idEnemyPair.Value;
+            break;
+
             //TODO: FIND CLOSEST ON SPLINE
         }
-
-        if ( target == Vector3.positiveInfinity ) return;
         
+        return target;
+    }
+    
+    private void sendProjectile(GameObject enemy) {
+        foreach ( Transform weaponPosition in _towerWeaponPosition ) {
+            Projectile _projectile = Instantiate(projectile, weaponPosition.position, weaponPosition.rotation);
+            _projectile.setEnemy(enemy);
+        }
+    }
+
+    private void LookAtEnemy() {
+        Vector3 enemyPosition = _enemyBeingTargetted.transform.position;
         foreach ( Transform weapon in _towerWeaponVisuals ) {
-            Vector3 targetPostition = new Vector3( target.x, 
+            Vector3 targetPostition = new Vector3( enemyPosition.x, 
                                                    weapon.position.y, 
-                                                   target.z ) ;
+                                                   enemyPosition.z ) ;
             weapon.LookAt( targetPostition ) ;
         }
     }
