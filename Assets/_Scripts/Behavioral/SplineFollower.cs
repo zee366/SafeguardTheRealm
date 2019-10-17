570d2a65eede0b77113947cda4d93be0ff4d47b5
Isaac Doré <swessmusic@gmail.com>
2019-10-17 08:58:37 -0400

    Entire SplineFollower logic

---
+++ Assets/_Scripts/Behavioral/SplineFollower.cs
@@ -0,0 +1,37 @@
+﻿using SplineMesh;
+using UnityEngine;
+
+namespace Behavioral {
+    public class SplineFollower : MonoBehaviour {
+
+        public float speedInUnitsPerSecond = 1.0f;
+
+
+        private Spline _splineRef;
+        private float  _posOnSpline = 0.0f;
+        private bool   _isOk        = true;
+
+
+        private void Awake() {
+            _splineRef = transform.parent.GetComponent<Spline>();
+
+            if ( !_splineRef ) {
+                Debug.LogWarning("Not child of a Spline. Can't follow...'");
+                _isOk = false;
+            }
+        }
+
+
+        void Update() {
+            _posOnSpline += Time.deltaTime * (speedInUnitsPerSecond / _splineRef.Length);
+            CurveSample sample = _splineRef.GetSample(_posOnSpline * (_splineRef.nodes.Count-1));
+            transform.position = sample.location;
+
+            Debug.Log(_posOnSpline * (_splineRef.nodes.Count-1));
+        }
+
+
+        public float GetPercentageOfSplineProgress() { return _posOnSpline; }
+
+    }
+}
