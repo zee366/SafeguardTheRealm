f5d8e4110a21199a05e1fe990ab33fcbf9df6011
Isaac Doré <swessmusic@gmail.com>
2019-10-20 14:31:57 -0400

    Snappable object structure

---
+++ Assets/_Scripts/SnapSystem/SnapLocation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace SnapSystem {
+    public class SnapLocation : MonoBehaviour {
+
+        private void OnDrawGizmos() {
+            Gizmos.DrawWireCube(  transform.position + Vector3.one/2 , new Vector3(1,0.5f, 1) );
+        }
+
+    }
+}
