8f98fe3ae4aef69e2cbf3ed35e1c625217a715a6
Isaac Doré <swessmusic@gmail.com>
2019-10-20 16:27:34 -0400

    Camera + Snap location Hover state

--- Assets/_Scripts/SnapSystem/SnapLocation.cs
+++ Assets/_Scripts/SnapSystem/SnapLocation.cs
@@ -1,11 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace SnapSystem {
     public class SnapLocation : MonoBehaviour {
 
-        private void OnDrawGizmos() {
-            Gizmos.DrawWireCube(  transform.position + Vector3.one/2 , new Vector3(1,0.5f, 1) );
+        [SerializeField] private GameObject highlightGameObject;
+
+        private bool _isLit = false;
+
+        public bool IsLit {
+            get { return _isLit; }
+            set {
+                _isLit = value;
+                OnHighlightStateChange();
+            }
         }
+
+
+        private void Start() {
+            OnHighlightStateChange();
+        }
+
+
+        /// <summary>
+        /// Internal event logic for state change
+        /// </summary>
+        private void OnHighlightStateChange() { highlightGameObject.SetActive(_isLit); }
+
+
+        /// <summary>
+        /// Simple box rendered in EditorMode to easily know where Tiles are located
+        /// </summary>
+        private void OnDrawGizmos() { Gizmos.DrawWireCube(transform.position + Vector3.one / 2, new Vector3(1, 0.5f, 1)); }
 
     }
 }
---
+++ Assets/_Scripts/SnapSystem/SnapManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace SnapSystem {
+    public class SnapManager : MonoBehaviour {
+
+        [SerializeField] private LayerMask selectableLayerMask;
+
+        private Camera       _cam;
+        private SnapLocation _currentTarget;
+        private SnapLocation _lastSelected;
+
+        private void Awake() { _cam = Camera.main; }
+
+
+        private void Update() {
+            // Check Mouse position for available action on Grid
+            CheckCurrentTarget(Input.mousePosition);
+
+            // Left click
+            if ( Input.GetMouseButtonDown(0) ) {
+
+                CheckLocationSelection();
+            }
+
+        }
+
+
+        /// <summary>
+        /// Mouse movement check for target.
+        /// Used for location highlighting and potential tile information gathering
+        /// </summary>
+        private void CheckCurrentTarget(Vector3 mousePos) {
+            RaycastHit hit;
+            Ray        ray = _cam.ScreenPointToRay(mousePos);
+
+            if ( Physics.Raycast(ray, out hit, Mathf.Infinity, selectableLayerMask) ) {
+                // Make sure the selectable has associated script
+                SnapLocation newRef = hit.transform.GetComponent<SnapLocation>();
+
+                // Change state of older one
+                if ( _currentTarget != null && _currentTarget != newRef )
+                    _currentTarget.IsLit = false;
+
+                _currentTarget = newRef;
+
+                // Make sure the new reference was indeed a SnapLocation.. maybe not
+                if ( _currentTarget == null )
+                    return;
+
+                _currentTarget.IsLit = true;
+
+            } else {
+                // We didn't hit this layer
+                if ( _currentTarget != null ) {
+                    _currentTarget.IsLit = false;
+                    _currentTarget = null;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Selectable selection logic
+        /// </summary>
+        private void CheckLocationSelection() { _lastSelected = _currentTarget; }
+
+    }
+}
