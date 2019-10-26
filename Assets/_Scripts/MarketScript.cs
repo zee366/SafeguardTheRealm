dd3de97f1fe78fd7d96b33d59a9455fb3ab90c59
CarlosAFroes <carlosalmeidafroes@gmail.com>
2019-10-26 14:26:18 -0400

    WIP Market SCr

--- Assets/_Scripts/MarketScript.cs
+++ Assets/_Scripts/MarketScript.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class MarketScript : MonoBehaviour
 {
     [SerializeField]
-    GameObject transformation;
+    GameObject player;
 
+    # mocking the player level
+    int playerLevel = 1;
+    # mocking the tower tier
+    char towerLevel;
 
+    # probability to show the towers
     List<int> level1 = new List<int> { 75, 25, 0 };
     List<int> level2 = new List<int> { 50, 45, 5 };
     List<int> level3 = new List<int> { 30, 55, 15 };
     List<int> level4 = new List<int> { 25, 45, 30 };
     List<int> level5 = new List<int> { 15, 40, 45 };
+
+
+    List<GameObject> towersA;
+    List<GameObject> towersB;
+    List<GameObject> towersC;
 
 
     // Start is called before the first frame update
@@ -24,12 +34,45 @@ public class MarketScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (randomValue() <= level1[0]) {
-            transform.Translate(Vector2.up * 1);
+        
+    }
+
+    private void getTowerLevel() {
+        int playaLevel = getPlayerLevel();
+        int wRandomNumber = randomValue();
+
+        switch (playaLevel) {
+            case 1:
+                if (wRandomNumber <= level1[0]) { towerLevel = 'A'; }
+                if (wRandomNumber > level1[0]) { towerLevel = 'B'; }
+                break;
+            case 2:
+                if (wRandomNumber <= level2[0]) { towerLevel = 'A'; }
+                if ((wRandomNumber > level2[0]) && (randomValue() <= level2[1]) { towerLevel = 'B'; }
+                if (wRandomNumber > level2[1]) { towerLevel = 'C'; }
+                break;
+            case 3:
+                if (wRandomNumber <= level3[0]) { towerLevel = 'A'; }
+                if ((wRandomNumber > level3[0]) && (randomValue() <= level3[1])) { towerLevel = 'B'; }
+                if (wRandomNumber > level3[1]) { towerLevel = 'C'; }
+                break;
+            case 4:
+                if (wRandomNumber <= level4[0]) { towerLevel = 'A'; }
+                if ((wRandomNumber > level4[0]) && (randomValue() <= level4[1])) { towerLevel = 'B'; }
+                if (wRandomNumber > level4[1]) { towerLevel = 'C'; }
+                break;
+            case 5:
+                if (wRandomNumber <= level5[0]) { towerLevel = 'A'; }
+                if ((wRandomNumber > level5[0]) && (randomValue() <= level5[1])) { towerLevel = 'B'; }
+                if (wRandomNumber > level5[1]) { towerLevel = 'C'; }
+                break;
         }
-        if (randomValue() > level1[0]) {
-            transform.Translate(-Vector2.up * 1);
-        }
+    }
+
+    private int whichTower() {
+        System.Random rand = new System.Random();
+        int x = rand.Next(1);
+        return x;
     }
 
     private int randomValue() {
@@ -37,5 +80,8 @@ public class MarketScript : MonoBehaviour
         int x = random.Next(101);
         return x;
     }
-    
+
+    private int getPlayerLevel() {
+        return playerLevel;
+    }
 }
