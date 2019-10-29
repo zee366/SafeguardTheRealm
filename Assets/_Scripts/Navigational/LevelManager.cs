1f8921eddbc714356d34a6895bbf87fad6c9b58a
vlbbucket <vincent.l.berardi@gmail.com>
2019-10-29 15:28:53 -0400

    UI features of Player and Market. Along with some testing scripts of player, market, inventory, and gamecontroller. Also, did some scenes for the game over and menu scene.

---
+++ Assets/_Scripts/Navigational/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Load scene based on name passed; register in console
+    public void LoadLevel(string sceneName)
+    {
+        Debug.Log("New Level Load: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Allow player to return to main menu scene once they click the button on the game over scene
+    // This function is associated with it via OnClick() event
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); //TODO make sure good name of menu scene
+    }
+}
