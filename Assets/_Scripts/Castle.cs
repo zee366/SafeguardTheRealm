d044f5903370cf95b2c2017a59a77aa1a5c1e296
Jason Brennan <brenjaso@gmail.com>
2019-10-29 17:24:30 -0400

    Added basic Castle script, and MoneyYielder script

---
+++ Assets/_Scripts/Castle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Castle : MonoBehaviour
+{
+    [SerializeField] int _health;
+
+    public UnityEvent onCastleHit;
+
+    void TakeDamage(int value) {
+        _health -= value;
+        onCastleHit?.Invoke();
+    }
+}
---
+++ Assets/_Scripts/MoneyYielder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MoneyYielder : MonoBehaviour
+{
+    int _winStreak { get; set; } = 0;
+    int _loseStreak { get; set; } = 0;
+
+    GameObject _player;
+
+    const int END_OF_ROUND_WINNINGS = 1;
+
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
+
+    public void GiveGoldToPlayer(int value) {
+        _player.GetComponent<Player>().gainGold(value);
+    }
+
+    public void ResetWinStreak() {
+        _winStreak = 0;
+        _loseStreak++;
+    }
+
+    public void ResetLoseStreak() {
+        _loseStreak = 0;
+        _winStreak++;
+    }
+
+    public void EndOfRoundWinnings() {
+        GiveGoldToPlayer((_winStreak + 1) * END_OF_ROUND_WINNINGS);
+    }
+}
