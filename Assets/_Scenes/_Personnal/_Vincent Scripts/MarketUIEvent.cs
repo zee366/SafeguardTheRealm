50578ef74128cd2b2941fab458dc150d735b5241
Isaac Doré <swessmusic@gmail.com>
2019-10-30 13:02:35 -0400

    Prep for Beginning of UI integration

--- Assets/_Scenes/_Personnal/_Vincent Scripts/MarketUIEvent.cs
+++ Assets/_Scenes/_Personnal/_Vincent Scripts/MarketUIEvent.cs
@@ -2,30 +2,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MarketUIEvent : MonoBehaviour
-{
+public class MarketUIEvent : MonoBehaviour {
+
     // Get associated player, inventory, and market objects
-    public TestingPlayer mPlayer;           //TODO change to actual class
-    public TestingInventory mInventory;     //TODO change to actual class
-    public TestingMarket mMarket;           //TODO change to actual class
+    public Player       player;    //TODO change to actual class
+    public Inventory    inventory; //TODO change to actual class
+    public MarketScript market;    //TODO change to actual class
 
     // Containers for chosen tower objects and their info
-    List<GameObject> mChosenTowers;         
-    List<string> mChosenTowersNames;
-    List<int> mChosenTowersCosts;
-    
+    List<GameObject> mChosenTowers;
+    List<string>     mChosenTowersNames;
+    List<int>        mChosenTowersCosts;
+
     // Cost of a reroll is always 1 gold
-    const int REROLL_COST = -1;
+    const  int  REROLL_COST = -1;
     public Text mRerollButtonText;
 
     // Get associated gain level button and its associated level cost text
     public Button mLevelUpButton;
+
     //public Button MLevelUpButton { get => mLevelUpButton; set => mLevelUpButton = value; }    //better?
     public Text mLevelUpAmountText;
 
     // Attributes associated with cost of gaining a level
     int mGainLevelCost = -2;
-
 
     // Attributes for storing the tower type and level
     string mTowerSlot1Info;
@@ -36,7 +36,7 @@ public class MarketUIEvent : MonoBehaviour
     public Text mTowerSlot1InfoText;
     public Text mTowerSlot2InfoText;
     public Text mTowerSlot3InfoText;
-    
+
     // Attributes for storing the tower cost
     int mTowerSlot1Cost;
     int mTowerSlot2Cost;
@@ -47,88 +47,93 @@ public class MarketUIEvent : MonoBehaviour
     public Text mTowerSlot2CostText;
     public Text mTowerSlot3CostText;
 
+
     // Start is called before the first frame update
-    private void Awake()
-    {
+    private void Awake() {
         // Initialize the chosen towers and the associated cost texts of the buttons
-        mChosenTowers = mMarket.GetThreeChosenTowers();
-        mRerollButtonText.text = "REROLL: " + REROLL_COST.ToString();
+        mChosenTowers           = market.GetTowers();
+        mRerollButtonText.text  = "REROLL: " + REROLL_COST.ToString();
         mLevelUpAmountText.text = mGainLevelCost.ToString();
         DisplayTowerSlotsInfo();
         DisplayTowerCostForButtons();
     }
 
+
     // Shows the info of each chosen tower in format of type and level
-    public void DisplayTowerSlotsInfo()
-    {
+    public void DisplayTowerSlotsInfo() {
         mTowerSlot1InfoText.text = mChosenTowersNames[0] + " " + mChosenTowersCosts[0].ToString();
         mTowerSlot1InfoText.text = mChosenTowersNames[1] + " " + mChosenTowersCosts[1].ToString();
         mTowerSlot1InfoText.text = mChosenTowersNames[2] + " " + mChosenTowersCosts[2].ToString();
     }
 
+
     // Shows the info of each chosen tower's cost for their appropriate button
-    public void DisplayTowerCostForButtons()
-    {
+    public void DisplayTowerCostForButtons() {
         mTowerSlot1CostText.text = mChosenTowersCosts[0].ToString();
         mTowerSlot2CostText.text = mChosenTowersCosts[1].ToString();
         mTowerSlot3CostText.text = mChosenTowersCosts[2].ToString();
     }
 
+
     // Allows player to generate a new list of towers to choose from
-    public void RerollTheMarket()
-    {
-        if (mPlayer.checkGold(REROLL_COST))     //check if player has enough gold to do this, if not, do nothing
+    public void RerollTheMarket() {
+        if ( player.CheckGold(REROLL_COST) ) //check if player has enough gold to do this, if not, do nothing
         {
-            mPlayer.removeGold(REROLL_COST);
-            mMarket.GenerateNewListOfTowers(ref mChosenTowers);
+            player.RemoveGold(REROLL_COST);
+            market.GenerateTowersList();
+            mChosenTowers = market.GetTowers();
+
             DisplayTowerSlotsInfo();
             DisplayTowerCostForButtons();
         }
     }
 
+
     // Following 3 functions will be selected based on the 3 buttons of the market
     // They will add the appropriate tower to the inventory if the player has enough gold to purchase them
-    public void AddTowerFromSlot1ToInventory()
-    {
-        if (mPlayer.checkGold(mChosenTowersCosts[0]))   //check if player has enough gold to do this, if not, do nothing
+    public void AddTowerFromSlot1ToInventory() {
+        if ( player.CheckGold(mChosenTowersCosts[0]) ) //check if player has enough gold to do this, if not, do nothing
         {
             //mInventory.Add(mChosenTowers[0]);
-            mPlayer.removeGold(mChosenTowersCosts[0]);
-        }
-    }
-    public void AddTowerFromSlot2ToInventory()
-    {
-        if (mPlayer.checkGold(mChosenTowersCosts[1]))   //check if player has enough gold to do this, if not, do nothing
-        {
-            //mInventory.Add(mChosenTowers[1]);
-            mPlayer.removeGold(mChosenTowersCosts[1]);
-        }
-    }
-    public void AddTowerFromSlot3ToInventory()
-    {
-        if (mPlayer.checkGold(mChosenTowersCosts[2]))   //check if player has enough gold to do this, if not, do nothing
-        {
-            //mInventory.Add(mChosenTowers[2]);
-            mPlayer.removeGold(mChosenTowersCosts[2]);
+            player.RemoveGold(mChosenTowersCosts[0]);
         }
     }
 
-    // Allows player to buy a new level
-    public void BuyPlayerLevel() //TODO change to actual player class name
-    {
-        if (mPlayer.checkGold(mGainLevelCost))    //check if player has enough gold to do this, if not, do nothing
+
+    public void AddTowerFromSlot2ToInventory() {
+        if ( player.CheckGold(mChosenTowersCosts[1]) ) //check if player has enough gold to do this, if not, do nothing
         {
-            mPlayer.gainLevel(mGainLevelCost);
+            //mInventory.Add(mChosenTowers[1]);
+            player.RemoveGold(mChosenTowersCosts[1]);
+        }
+    }
+
+
+    public void AddTowerFromSlot3ToInventory() {
+        if ( player.CheckGold(mChosenTowersCosts[2]) ) //check if player has enough gold to do this, if not, do nothing
+        {
+            //mInventory.Add(mChosenTowers[2]);
+            player.RemoveGold(mChosenTowersCosts[2]);
+        }
+    }
+
+
+    // Allows player to buy a new level
+    public void BuyPlayerLevel() {
+        if ( player.CheckGold(mGainLevelCost) ) //check if player has enough gold to do this, if not, do nothing
+        {
+            player.GainLevel(mGainLevelCost);
             mGainLevelCost--;
         }
     }
 
+
     // TODO
     // Is following function to be handled here or in gamecontroller?
     // Function for when player is done using the market UI
-    public void SelectedDone()
-    {
+    public void SelectedDone() {
         // Logic for when button done is pressed
         // Handle here or in game controller?
     }
+
 }
--- Assets/_Scripts/MarketScript.cs
+++ Assets/_Scripts/MarketScript.cs
@@ -2,39 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class MarketScript : MonoBehaviour {
 
-public class MarketScript : MonoBehaviour
-{
-   
     Player player;
-    
-   
+
     // It will define the probability to all the player levels and will be accessed by the inspector
-    [SerializeField]
-    List<Vector3> playerLevelTiersProbabilities = new List<Vector3>();
+    [SerializeField] List<Vector3> playerLevelTiersProbabilities = new List<Vector3>();
+
     // the tier struct with the price and the tower list
-    [SerializeField]
-    List<TowerTier> tiers = new List<TowerTier>();
+    [SerializeField] List<TowerTier> tiers = new List<TowerTier>();
+
     // the towers game objects
-    [SerializeField]
-    List<GameObject> towers = new List<GameObject>();
+    [SerializeField] List<GameObject> towers = new List<GameObject>();
 
     System.Random rand = new System.Random();
 
     // used for testing
-    private float _t = 0;
+    private float    _t        = 0;
     List<GameObject> instances = new List<GameObject>();
 
+
     // Start is called before the first frame update
-    void Start()
-    {
-        addTowers();
-        spawnTowers();
-    }
+    void Start() { GenerateTowersList(); }
+
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         /* used for testing
         if (_t > 3f)
         {
@@ -46,62 +39,73 @@ public class MarketScript : MonoBehaviour
         */
     }
 
-    // will add the towers 
-    private void addTowers()
-    {
+
+    // will add the towers
+    public void GenerateTowersList() {
         /* used for testing
         foreach (var inst in instances)
             Destroy(inst);
             */
 
-        
+
         towers = new List<GameObject>();
-        for (int i = 0; i < 3; i++)
-        {
-            int index = getTowerLevel();
+        for ( int i = 0; i < 3; i++ ) {
+            int index            = getTowerLevel();
             int tiersTowerRandom = Random.Range(0, tiers[index].towers.Count);
             towers.Add(tiers[index].towers[tiersTowerRandom]);
         }
-
     }
+
+
     // spawn the towers in the scene
     private void spawnTowers() {
         int x = 2;
         int y = 2;
-        foreach (GameObject tower in towers) {
+        foreach ( GameObject tower in towers ) {
             Instantiate(tower, new Vector2(x, y), Quaternion.identity);
             //instances.Add(Instantiate(tower, new Vector2(x, y), Quaternion.identity));
             x++;
             y++;
         }
     }
+
+
     // tells which tower tier was weighted choosen
     private int getTowerLevel() {
-        int playaLevel = getPlayerLevel();
+        int playaLevel    = getPlayerLevel();
         int wRandomNumber = randomValue();
         //Debug.Log(wRandomNumber);
 
         float tierOneEndPointProbability = playerLevelTiersProbabilities[playaLevel].x;
         float tierTwoEndPointProbability = tierOneEndPointProbability + playerLevelTiersProbabilities[playaLevel].y;
-        
 
-        if (wRandomNumber <= tierOneEndPointProbability)
+
+        if ( wRandomNumber <= tierOneEndPointProbability )
             return 0;
-        if (wRandomNumber > tierOneEndPointProbability && wRandomNumber <= tierTwoEndPointProbability) 
+
+        if ( wRandomNumber > tierOneEndPointProbability && wRandomNumber <= tierTwoEndPointProbability )
             return 1;
-        if (wRandomNumber > tierTwoEndPointProbability)
+
+        if ( wRandomNumber > tierTwoEndPointProbability )
             return 2;
 
         return 0;
     }
 
+
     private int randomValue() {
         int x = rand.Next(101);
         return x;
     }
+
+
     //WIP
     private int getPlayerLevel() {
         //Player.level;
         return 3;
     }
+
+
+    public List<GameObject> GetTowers() { return towers; }
+
 }
