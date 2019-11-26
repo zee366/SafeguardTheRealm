fd50435178000078b4bef7529b176750003023da
Jason Brennan <brenjaso@gmail.com>
2019-11-25 20:03:25 -0500

    Added water mill with particle effects to village. Added lights to castle. Started creating spline for left path

---
+++ Assets/_Scripts/WaterMill.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMill : MonoBehaviour
+{
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(90.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
