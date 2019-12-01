20b396c8a5f1283f784c411e91a9e876d3b6e907
yannhua <bagabagel@hotmail.com>
2019-12-01 18:01:34 -0500

    Modified "MoneyYielder"

--- Assets/_Scripts/MoneyYielder.cs
+++ Assets/_Scripts/MoneyYielder.cs
@@ -2,10 +2,16 @@
 
 public class MoneyYielder : MonoBehaviour
 {
-    const float INTEREST = 0.25f;
+    const float INTEREST_RATE = 0.25f;
+    int castle_MAX_HP;
+    GameObject castle;
+    Castle c;
 
     int _winStreak { get; set; } = 0;
     int _loseStreak { get; set; } = 0;
+
+    // get castle's hp at beginning of the round
+    int old_castleHP { get; set; } = 0;
 
     Player _player;
 
@@ -14,6 +20,16 @@ public class MoneyYielder : MonoBehaviour
     void Start()
     {
         _player = GetComponent<Player>();
+        castle = GameObject.Find("Castle");
+        c = castle.GetComponent<Castle>();
+        castle_MAX_HP = c.GetHealth();
+    }
+
+    // Set castleHP at begging of the round
+    // Call this method on "On Start Wave" Unity Event
+    public void SetCastleHP()
+    {
+        old_castleHP = c.GetHealth();
     }
 
     public void GiveGoldToPlayer(int value) {
@@ -30,11 +46,42 @@ public class MoneyYielder : MonoBehaviour
         _winStreak++;
     }
 
+    // Determine WinStreak/LoseStreak
+    public void CheckStreak()
+    {
+        int new_castleHP = c.GetHealth();
+        int x = old_castleHP - new_castleHP;
+
+        Debug.Log("old hp =" + old_castleHP);
+        Debug.Log("new hp =" + new_castleHP);
+        Debug.Log("X =" + x);
+        Debug.Log("% =" + (x * 1.0) / (castle_MAX_HP * 1.0));
+
+        // If at the end of the round, castle's hp does not lose more than 10% of total hp, increments win streak and reset lose streak
+        if ((x * 1.0) / (castle_MAX_HP * 1.0) < 0.1f)
+        {
+            ResetLoseStreak();
+        }
+        // If at the end of the round, castle's hp loses 10% or more of total hp, increments lose streak and reset win streak
+        else if ((x * 1.0) / (castle_MAX_HP * 1.0) >= 0.1)
+        {
+            ResetWinStreak();
+        }
+    }
+
     public void EndOfRoundWinnings() {
-        //GiveGoldToPlayer((++_winStreak) * END_OF_ROUND_WINNINGS);
+        // Check streak first
+        CheckStreak();
+        Debug.Log("Win =" + _winStreak);
+        Debug.Log("Lose =" + _loseStreak);
 
         // Calculate the interest then give the total to the player
-        int gold = _player.GetPlayerGold() + ((++_winStreak) * END_OF_ROUND_WINNINGS);
-        GiveGoldToPlayer(Mathf.RoundToInt(gold * INTEREST) + ((_winStreak) * END_OF_ROUND_WINNINGS));
+        int bonus = Mathf.Abs(_winStreak - _loseStreak) * END_OF_ROUND_WINNINGS;
+        Debug.Log("Bonus =" + bonus);
+        int total = _player.GetPlayerGold() + bonus;
+        Debug.Log("Total =" + total);
+        int interest = Mathf.RoundToInt(total * INTEREST_RATE);
+        Debug.Log("Intereset =" + interest);
+        GiveGoldToPlayer(interest + bonus);
     }
 }
