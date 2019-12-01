3edc0390f034844ee8fa88ae51b95cac1274d837
Carlos <carlosalmeidafroes@gmail.com>
2019-11-30 22:24:00 -0500

    WIP for fade out transitions

--- Assets/_Scripts/Navigational/LevelManager.cs
+++ Assets/_Scripts/Navigational/LevelManager.cs
@@ -1,16 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public int sceneIndex;
+    Animator animator;
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        //change scene when user presses Space key
+        
+    }
+    private IEnumerator LoadSceneAFterTransition()
+    {
+        //show animate out animation
+        animator.SetBool("animateOut", true);
+        yield return new WaitForSeconds(1f);
+        //load the scene we want
+        //SceneManager.LoadScene(sceneIndex);
+    }
+
     // Load scene based on name passed; register in console
     public void LoadLevel(string sceneName)
     {
         Debug.Log("New Level Load: " + sceneName);
+        StartCoroutine(LoadSceneAFterTransition());
         SceneManager.LoadScene(sceneName);
     }
 
-    // Allow player to return to main menu scene once they click the button on the game over scene
+    //Allow player to return to main menu scene once they click the button on the game over scene
     // This function is associated with it via OnClick() event
     public void ReturnToMenu()
     {
