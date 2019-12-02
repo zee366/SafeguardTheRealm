4566b207c28001b88e0cf85a2e3a34a857062ef0
Isaac Doré <swessmusic@gmail.com>
2019-12-01 23:40:40 -0500

    Simple fix

--- Assets/_Scripts/UI/Market/MarketUIEvent.cs
+++ Assets/_Scripts/UI/Market/MarketUIEvent.cs
@@ -24,7 +24,7 @@ public class MarketUIEvent : MonoBehaviour {
 
 
     // Start is called before the first frame update
-    private void Awake() {
+    private void Start() {
         // References to slots + events
         _slots = GetComponentsInChildren<MarketSlot>();
         foreach ( MarketSlot slot in _slots ) {
